490c418febdba5e640d491910c67d9fa28ee6db3
dhayman <dhayman117@gmail.com>
2024-02-22 22:26:07 -0500

    Implement Devise Authentication

---
+++ Server/app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+
+  validates :username, uniqueness: true, length: { minimum: 4 }
+
+  validates :password_confirmation, presence: true, on: :create
+
+  #for good measure because I can't stress how much email is not a thing for an internal service running on an intranet
+  def email_required?
+    false
+   end
+  
+   def will_save_change_to_email?
+    false
+   end
+  
+   def email_changed?
+    false
+   end
+end
--- Server/config/routes.rb
+++ Server/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,5 +15,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: proc { [404, {}, ["Not found."]] }
 end
